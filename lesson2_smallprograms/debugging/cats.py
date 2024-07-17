add7fb3646881e9dd143ea607fb7003c3877739e
harutmark <harutmark@gmail.com>
2024-07-17 23:21:54 +0000

    Finished Assignments 13-15 on Debugging

---
+++ lesson2_smallprograms/debugging/cats.py
@@ -0,0 +1,5 @@
+cats = []
+for name in ('Powder', 'Sky', 'Cheddar', 'Cocoa',):
+    cats.append(name)
+    
+print(cats) #Expected Output: ['Powder', 'Sky', 'Cheddar', 'Cocoa']
---
+++ lesson2_smallprograms/debugging/debug.py
@@ -0,0 +1,9 @@
+# debug.py
+
+import pdb
+
+counter = 1
+
+while counter <= 5:
+    print(counter)
+    counter += 1
