e5003cbafb25c147a19b7bb60f97fe9ca2612285
harutmark <harutmark@gmail.com>
2024-08-01 23:36:58 +0000

    Multiple language functionality now added to calculator. Armenian translations added.

--- lesson2_smallprograms/calculator/calculator.py
+++ lesson2_smallprograms/calculator/calculator.py
@@ -10,46 +10,41 @@ with open('calculator_messages.json', 'r') as file:
 # Now 'MESSAGES' contains the loaded messages as a Python dictionary
 
 def prompt(message):
-    '''
-    Adds an '==> ' prefix to all terminal outputs
-    '''
     print(f"==> {message}")
 
 def invalid_number(number_str):
-    '''
-    Checks to see if user input is invalid (not float or integer)
-    Invalid inputs return True, Valid inputs return False
-    Valid = float or integer, Invalid = Not Float or Integer
-    '''
     try:
         float(number_str)
     except ValueError:
         return True
     return False
 
-prompt(MESSAGES['welcome'])
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
+
+prompt(messages('welcome'))
 
 while True:
-    prompt(MESSAGES["request_first_number"])
+    prompt(messages("request_first_number"))
     number1 = input('==> ')
     
     while invalid_number(number1):
-        prompt(MESSAGES["request_valid_number"])
+        prompt(messages("request_valid_number"))
         number1 = input('==> ')
     
-    prompt(MESSAGES["request_second_number"])
+    prompt(messages("request_second_number"))
     number2 = input('==> ')
     
     while invalid_number(number2):
-        prompt(MESSAGES["request_valid_number"])
+        prompt(messages("request_valid_number"))
         number2 = input('==> ')
     
-    prompt(MESSAGES["request_operation"])
-    prompt(MESSAGES["operation_options"])
+    prompt(messages("request_operation"))
+    prompt(messages("operation_options"))
     operation = input('==> ')
     
     while operation not in  ["+", "-", "*", "/"]:
-        prompt(MESSAGES["operation_options_2"])
+        prompt(messages("operation_options_2"))
         operation = input('==> ')
     
     match operation:
@@ -61,7 +56,7 @@ while True:
             OUTPUT = float(number1) * float(number2)
         case "/":
             if number2 == '0':
-                prompt(MESSAGES["zero_division_warning"])
+                prompt(messages("zero_division_warning"))
                 OUTPUT = None
             else:
                 OUTPUT = float(number1) / float(number2)
@@ -69,10 +64,8 @@ while True:
     prompt(f"{number1} {operation} {number2} = {OUTPUT}\n")
 
     #Another Calculation?
-    prompt(MESSAGES["another_calculation"])
-    prompt(MESSAGES["yes_or_no"])
+    prompt(messages("another_calculation"))
+    prompt(messages("yes_or_no"))
     another_calculation = input('==> ')
     if another_calculation[0].lower() != 'y':
         break
-    else:
-        continue
