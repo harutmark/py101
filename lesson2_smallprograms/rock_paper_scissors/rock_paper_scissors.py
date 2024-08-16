4ca0716b9ced5fdf9a8f387850a791e530094f61
harutmark <harutmark@gmail.com>
2024-08-16 21:26:12 +0000

    Updated rock_paper_scissors.py so the user can type 'r' for 'rock,' 'p' for 'paper,' and 's' for 'scissors.'

--- lesson2_smallprograms/rock_paper_scissors/rock_paper_scissors.py
+++ lesson2_smallprograms/rock_paper_scissors/rock_paper_scissors.py
@@ -4,7 +4,15 @@ A simple terminal game of Rock, Paper, Scissors against a randomized computer.
 
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+def is_valid(user_choice):
+    if user_choice.lower().startswith('r'):
+        return True
+    elif user_choice.lower().startswith('p'):
+        return True
+    elif user_choice.lower().startswith('s'):
+        return True
+    else:
+        return False
 
 def prompt(message):
     '''
@@ -14,35 +22,40 @@ def prompt(message):
     
 def display_winner(user_choice, computer_choice):
     '''
-    Displays user choice and computer choice, then determines winner
+    Displays user and computer choice, then displays winner
     '''
     prompt(f"You chose {user_choice}. The computer chose {computer_choice}.")
     
-    if ((user_choice == 'rock' and computer_choice == 'scissors') or #User Wins
+    if ((user_choice == 'rock' and computer_choice == 'scissors') or  #User Wins
         (user_choice == 'paper' and computer_choice == 'rock') or
         (user_choice == 'scissors' and computer_choice == 'paper')):
         prompt("Congratulations, you win!")
-    elif ((user_choice == 'rock' and computer_choice == 'paper') or  #CPU wins
+    elif ((user_choice == 'rock' and computer_choice == 'paper') or   #CPU wins
         (user_choice == 'paper' and computer_choice == 'scissors') or
         (user_choice == 'scissors' and computer_choice == 'rock')):
         prompt("Sorry, you lose.")
-    elif (user_choice == computer_choice):                           #Tie
+    elif (user_choice == computer_choice):                            # Tie
         prompt("Uh oh, seems like a tie!")
-    else:
+    else:                                                             # Error
         prompt("Woah! Seems like something went wrong.")
 
-   
 while True:
-    #User Makes Selection 
-    prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
+    prompt(f'Choose one: "rock," "paper," or "scissors"')
     user_choice = input("==> ")
     
-    while user_choice not in VALID_CHOICES:     #Ensure selection is valid
+    while is_valid(user_choice) == False:
         prompt("That's not a valid choice")
         prompt('Please choose between "rock", "paper", or "scissors"')
         user_choice = input("==> ")
+        
+    if user_choice.lower().startswith('r'):
+        user_choice = 'rock'
+    elif user_choice.lower().startswith('p'):
+        user_choice = 'paper'
+    elif user_choice.lower().startswith('s'):
+        user_choice = 'scissors'
     
-    computer_choice = random.choice(VALID_CHOICES)
+    computer_choice = random.choice(['rock', 'paper', 'scissors'])
     
     display_winner(user_choice, computer_choice)
     
